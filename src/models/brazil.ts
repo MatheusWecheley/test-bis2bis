0b9a796fb9139095ff4229e2e5a24056867de687
Matheus Wecheley <matheuswech@outlook.com>
2022-09-01 16:23:27 -0300

    resolved problem with middleware fetch countries

--- src/models/brazil.ts
+++ src/models/brazil.ts
@@ -7,149 +7,121 @@ import { Paraguai } from "../countries/paraguai/paraguai";
 import { Peru } from "../countries/peru/peru";
 import { Suriname } from "../countries/suriname/suriname";
 import { Uruguai } from "../countries/uruguay/uruguay";
+import { RequestHandler } from "express";
 
 export class Countries {
-    async brazil() {
-        await fetch('http://universities.hipolabs.com/search?country=brazil')
+    brazil: RequestHandler = async (req,res, next) => {
+        try {
+            await fetch('http://universities.hipolabs.com/search?country=brazil')
             .then((response: { json: () => any; }) => response.json())
             .then((response: any) => response.map(async (country: any) => {
                 const countryVerify = await Brazil.findOne({
                     university: country.name
                 });
-                if (countryVerify) {
-                    throw new Error('Univeristy has existed');
+                if (!countryVerify) {
+                    const newCountry = new Brazil({
+                        university: country.name
+                    })
+                    await newCountry.save();
+                    return newCountry;
                 }
-                const newCountry = new Brazil({
-                    university: country.name
-                })
-                await newCountry.save();
-                return console.log(newCountry);
-            }))
+            },
+            res.json(`sucess`)
+            ))
+        } catch (error) {
+            res.send(400)
+        }
     }
 
-    async argentina() {
-        await fetch('http://universities.hipolabs.com/search?country=argentina')
+    argentina: RequestHandler = async(req,res,next) => {
+        try {
+            await fetch('http://universities.hipolabs.com/search?country=argentina')
             .then((response: { json: () => any; }) => response.json())
             .then((response: any) => response.map(async (country: any) => {
                 const countryVerify = await Argentina.findOne({
                     university: country.name
                 });
-                if (countryVerify) {
-                    throw new Error('Univeristy has existed');
+                if (!countryVerify) {
+                    const newCountry = new Argentina({
+                        university: country.name
+                    })
+                    await newCountry.save();
+                    return newCountry;
                 }
-                const newCountry = new Argentina({
-                    university: country.name
-                })
-                await newCountry.save();
-                return console.log(newCountry);
-            }))
+            },
+            res.json(`sucess`)
+            ))
+        } catch (error) {
+            res.send(400)
+        }
     }
 
-    async chile() {
-        await fetch('http://universities.hipolabs.com/search?country=chile')
+    chile: RequestHandler = async (req, res, next) => {
+        try {
+            await fetch('http://universities.hipolabs.com/search?country=chile')
             .then((response: { json: () => any; }) => response.json())
             .then((response: any) => response.map(async (country: any) => {
                 const countryVerify = await Chile.findOne({
                     university: country.name
                 });
-                if (countryVerify) {
-                    throw new Error('Univeristy has existed');
+                if (!countryVerify) {
+                    const newCountry = new Chile({
+                        university: country.name
+                    })
+                    await newCountry.save();
+                    return newCountry;
                 }
-                const newCountry = new Chile({
-                    university: country.name
-                })
-                await newCountry.save();
-                return console.log(newCountry);
-            }))
+            },
+            res.json(`sucess`)
+            ))
+        } catch (error) {
+            res.send(400)
+        }
     }
 
-    async colombia() {
-        await fetch('http://universities.hipolabs.com/search?country=colombia')
-            .then((response: { json: () => any; }) => response.json())
-            .then((response: any) => response.map(async (country: any) => {
-                const countryVerify = await Colombia.findOne({
-                    university: country.name
-                });
-                if (countryVerify) {
-                    throw new Error('Univeristy has existed');
-                }
-                const newCountry = new Colombia({
-                    university: country.name
-                })
-                await newCountry.save();
-                return console.log(newCountry);
-            }))
-    }
-
-    async paraguai() {
-        await fetch('http://universities.hipolabs.com/search?country=paraguai')
-            .then((response: { json: () => any; }) => response.json())
-            .then((response: any) => response.map(async (country: any) => {
-                const countryVerify = await Paraguai.findOne({
-                    university: country.name
-                });
-                if (countryVerify) {
-                    throw new Error('Univeristy has existed');
-                }
-                const newCountry = new Paraguai({
-                    university: country.name
-                })
-                await newCountry.save();
-                return console.log(newCountry);
-            }))
-    }
-
-    async peru() {
-        await fetch('http://universities.hipolabs.com/search?country=peru')
-            .then((response: { json: () => any; }) => response.json())
-            .then((response: any) => response.map(async (country: any) => {
-                const countryVerify = await Peru.findOne({
-                    university: country.name
-                });
-                if (countryVerify) {
-                    throw new Error('Univeristy has existed');
-                }
-                const newCountry = new Peru({
-                    university: country.name
-                })
-                await newCountry.save();
-                return console.log(newCountry);
-            }))
-    }
-
-    async suriname() {
-        await fetch('http://universities.hipolabs.com/search?country=suriname')
+    suriname: RequestHandler = async (req, res, next) => {
+        try {
+            await fetch('http://universities.hipolabs.com/search?country=suriname')
             .then((response: { json: () => any; }) => response.json())
             .then((response: any) => response.map(async (country: any) => {
                 const countryVerify = await Suriname.findOne({
                     university: country.name
                 });
-                if (countryVerify) {
-                    throw new Error('Univeristy has existed');
+                if (!countryVerify) {
+                    const newCountry = new Suriname({
+                        university: country.name
+                    })
+                    await newCountry.save();
+                    return newCountry;
                 }
-                const newCountry = new Suriname({
-                    university: country.name
-                })
-                await newCountry.save();
-                return console.log(newCountry);
-            }))
+            },
+            res.json(`sucess`)
+            ))
+        } catch (error) {
+            res.send(400)
+        }
     }
 
-    async uruguay() {
-        await fetch('http://universities.hipolabs.com/search?country=uruguay')
+     uruguay: RequestHandler = async(req, res, next) => {
+        try {
+            await fetch('http://universities.hipolabs.com/search?country=uruguay')
             .then((response: { json: () => any; }) => response.json())
             .then((response: any) => response.map(async (country: any) => {
                 const countryVerify = await Uruguai.findOne({
                     university: country.name
                 });
-                if (countryVerify) {
-                    throw new Error('Univeristy has existed');
+                if (!countryVerify) {
+                    const newCountry = new Uruguai({
+                        university: country.name
+                    })
+                    await newCountry.save();
+                    return newCountry;
                 }
-                const newCountry = new Uruguai({
-                    university: country.name
-                })
-                await newCountry.save();
-                return console.log(newCountry);
-            }))
+            },
+            res.json(`sucess`)
+            ))
+        } catch (error) {
+            res.send(400)
+        }
     }
 }
--- src/server.ts
+++ src/server.ts
@@ -5,16 +5,22 @@ import authenticate  from './database/database';
 const app = express();
 const database = authenticate()
 
-const country = new Countries();
-country.brazil();
-country.argentina();
-country.chile();
-country.colombia();
-country.paraguai();
-country.peru();
-country.suriname();
-// country.uruguay();
+const country = new Countries;
+app.use('/', country.argentina)
+// const country = new Countries();
+// country.brazil();
+// country.argentina();
+// country.chile();
+// country.colombia();
+// country.paraguai();
+// country.peru();
+// country.suriname();
+// // country.uruguay();
 
-app.listen(3000, () => {
-    console.log("Server Running");
-})
+try {
+    app.listen(3000, () => {
+        console.log("Server Running");
+    })
+} catch (error) {
+    
+}
